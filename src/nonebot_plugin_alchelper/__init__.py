c57a8282575c981098bb115ed8f44b89f5224356
RF-Tar-Railt <rf_tar_railt@qq.com>
2024-12-31 13:19:56 +0800

    :arrow_up: version 0.6.0

--- src/nonebot_plugin_alchelper/__init__.py
+++ src/nonebot_plugin_alchelper/__init__.py
@@ -26,10 +26,8 @@ from . import migrations
 from .config import Config
 from .model import Record
 
-driver = get_driver()
-global_config = driver.config
 _config = get_plugin_config(Config)
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 __plugin_meta__ = PluginMetadata(
     name="Alconna 帮助工具",
     description="基于 nonebot-plugin-alconna，给出所有命令帮助以及统计",
@@ -51,38 +49,41 @@ __plugin_meta__ = PluginMetadata(
     },
 )
 
-with namespace("alchelper") as ns:
-    ns.prefixes = list(_config.alchelper_command_start or global_config.command_start)
+prefixes = []
+if _config.alchelper_command_start:
+    prefixes = list(_config.alchelper_command_start)
 
-    _help = Alconna(
-        _config.alchelper_help,
-        Args["page", int, 0],
-        Option("--hide", help_text="是否列出隐藏命令", action=store_true, default=False),
-        meta=CommandMeta(
-            description="显示所有命令帮助",
-            usage="可以使用 --hide 参数来显示隐藏命令",
-            example=f"${_config.alchelper_help} 1",
-        ),
-    )
-    _help.shortcut("帮助", {"prefix": True, "fuzzy": False})
-    _help.shortcut("所有帮助", {"args": ["--hide"], "prefix": True, "fuzzy": False})
-    _help.shortcut(r"第(\d+)页帮助", {"args": ["{0}"], "prefix": True, "fuzzy": False})
-    _statis = Alconna(
-        _config.alchelper_statis,
-        Arg("type", Literal["show", "most", "least", "user"], "most"),
-        Arg("count", int, _config.alchelper_statis_msgcount_default),
-        Option("--clear", help_text="清除所有记录", action=store_true, default=False),
-        Option("--user-id", Args["user_id", [str, At]], help_text="统计用户的记录"),
-        meta=CommandMeta(
-            description="命令统计",
-            example=f"${_config.alchelper_statis} msg",
-        ),
-    )
-    _statis.shortcut("消息统计", {"args": ["show"], "prefix": True})
-    _statis.shortcut("命令统计", {"args": ["most"], "prefix": True})
-    _statis.shortcut("清除统计", {"args": ["--clear"], "prefix": True, "fuzzy": False})
-    _statis.shortcut("用户(.+)统计", {"args": ["user", "--user-id", "{0}"], "prefix": True, "fuzzy": False, "humanized": "用户<用户id>统计"})
-    _statis.shortcut("用户统计", {"args": ["user", "--user-id", "{%0}"], "prefix": True, "humanized": "用户统计 <用户id>"})
+_help = Alconna(
+    prefixes,
+    _config.alchelper_help,
+    Args["page", int, 0],
+    Option("--hide", help_text="是否列出隐藏命令", action=store_true, default=False),
+    meta=CommandMeta(
+        description="显示所有命令帮助",
+        usage="可以使用 --hide 参数来显示隐藏命令",
+        example=f"${_config.alchelper_help} 1",
+    ),
+)
+_help.shortcut("帮助", {"prefix": True, "fuzzy": False})
+_help.shortcut("所有帮助", {"args": ["--hide"], "prefix": True, "fuzzy": False})
+_help.shortcut(r"第(\d+)页帮助", {"args": ["{0}"], "prefix": True, "fuzzy": False})
+_statis = Alconna(
+    prefixes,
+    _config.alchelper_statis,
+    Arg("type", Literal["show", "most", "least", "user"], "most"),
+    Arg("count", int, _config.alchelper_statis_msgcount_default),
+    Option("--clear", help_text="清除所有记录", action=store_true, default=False),
+    Option("--user-id", Args["user_id", [str, At]], help_text="统计用户的记录"),
+    meta=CommandMeta(
+        description="命令统计",
+        example=f"${_config.alchelper_statis} msg",
+    ),
+)
+_statis.shortcut("消息统计", {"args": ["show"], "prefix": True})
+_statis.shortcut("命令统计", {"args": ["most"], "prefix": True})
+_statis.shortcut("清除统计", {"args": ["--clear"], "prefix": True, "fuzzy": False})
+_statis.shortcut("用户(.+)统计", {"args": ["user", "--user-id", "{0}"], "prefix": True, "fuzzy": False, "humanized": "用户<用户id>统计"})
+_statis.shortcut("用户统计", {"args": ["user", "--user-id", "{%0}"], "prefix": True, "humanized": "用户统计 <用户id>"})
 
 
 class HelperExtension(Extension):
@@ -97,13 +98,13 @@ class HelperExtension(Extension):
     async def parse_wrapper(self, bot, state, event, res: Arparma) -> None:
         if res.source != _statis.path:
             async with get_session() as session:
-                session.add(Record(source=res.source, origin=str(res.origin), sender=str(event.get_user_id())))
+                session.add(Record(source=res.source.path, origin=str(res.origin), sender=str(event.get_user_id())))
                 await session.commit()
 
 add_global_extension(HelperExtension())
 
-help_cmd = on_alconna(_help, auto_send_output=True)
-statis_cmd = on_alconna(_statis, auto_send_output=True)
+help_cmd = on_alconna(_help, auto_send_output=True, use_cmd_start=not prefixes)
+statis_cmd = on_alconna(_statis, auto_send_output=True, use_cmd_start=not prefixes)
 
 
 @help_cmd.handle()
--- src/nonebot_plugin_alchelper/model.py
+++ src/nonebot_plugin_alchelper/model.py
@@ -1,14 +1,16 @@
 from datetime import datetime
+from dataclasses import dataclass
 from nonebot_plugin_orm import Model
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import DateTime
 
 
+@dataclass
 class Record(Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    source: Mapped[str]
-    origin: Mapped[str]
-    sender: Mapped[str]
+    id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True, unique=True)
+    source: Mapped[str] = mapped_column()
+    origin: Mapped[str] = mapped_column()
+    sender: Mapped[str] = mapped_column()
     time: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
 
     __tablename__  = "alchelper_record"
